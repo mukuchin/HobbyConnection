38bc89dc6d762b91c99663b052fad89017802be6
mukuchin <haruto.mukuno@gmail.com>
2023-08-02 18:47:54 +0900

    各ファイルを前のアプリから移動させた。バリデーションは後で実装する。

--- routes/web.php
+++ routes/web.php
@@ -29,10 +29,21 @@ Route::get('/dashboard', function () {
     return Inertia::render('Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::controller(ArticlesController::class)->middleware(['auth'])->group(function () {
+    Route::get('/index', 'top')->name('top'); //TOPページ表示
+    Route::post('/posts', 'store')->name('store'); //保存処理
+    Route::get('/posts/create', 'create')->name('create'); //記事の新規投稿
+    Route::get('/posts/myposts', 'myposts')->name('myposts'); //自分が投稿した記事の一覧
+    Route::get('/posts/{article}', 'show')->name('show'); //記事の閲覧
+    Route::put('/posts/{article}', 'update')->name('update'); //記事の更新
+    Route::delete('/posts/{article}', 'delete')->name('delete'); //記事の削除
+    Route::get('/posts/{article}/edit', 'edit')->name('edit'); //記事の編集
+});
+
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
