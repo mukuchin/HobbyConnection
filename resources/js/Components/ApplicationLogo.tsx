942a074f26df30488205e41e9e6c74eef71bf726
mukuchin <haruto.mukuno@gmail.com>
2023-09-30 18:14:07 +0900

    ボタンのデザインと記事閲覧ページのロゴが表示されない不具合を修正した。

--- resources/js/Components/ApplicationLogo.tsx
+++ resources/js/Components/ApplicationLogo.tsx
@@ -1,7 +1,7 @@
 export default function ApplicationLogo() {
     return (
         <img
-            src="images/HobbyConnectionLogo3.png"
+            src="/images/HobbyConnectionLogo3.png"
             alt="Application Logo"
             className="w-50 h-20"
         />
