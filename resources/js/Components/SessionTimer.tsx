4a177a2ed252899dd2b32db25354fa2f8071927b
mukuchin <haruto.mukuno@gmail.com>
2023-10-14 00:05:19 +0900

    フォームにタイマーを設定した。また、フォームに入力した後に遷移や再読み込みする際に警告するようにした。

---
+++ resources/js/Components/SessionTimer.tsx
@@ -0,0 +1,43 @@
+import React, { useState, useEffect } from "react";
+
+const SessionTimer: React.FC = () => {
+    const sessionDuration = 120 * 60; // 120分を秒に変換
+    const startTime = new Date().getTime();
+
+    const [elapsedTime, setElapsedTime] = useState(0);
+
+    // タイマーの表示
+    const showTimer = () => {
+        const currentTime = new Date().getTime();
+        const elapsedTimeSec = Math.floor((currentTime - startTime) / 1000);
+
+        setElapsedTime(elapsedTimeSec);
+
+        // 残り時間のアラート
+        const alertTimes = [3600, 1800, 600, 300, 60];
+        if (alertTimes.includes(sessionDuration - elapsedTimeSec)) {
+            alert(`残り時間: ${sessionDuration - elapsedTimeSec}秒です！`);
+        }
+
+        setTimeout(showTimer, 10);
+    };
+
+    useEffect(() => {
+        showTimer();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
+
+    const timeLeft = sessionDuration - elapsedTime;
+    const hours = Math.floor(timeLeft / 3600);
+    const minutes = Math.floor((timeLeft % 3600) / 60);
+    const seconds = timeLeft % 60;
+
+    return (
+        <div className="text-lg text-gray-500 mb-2 font-noto-sans-jp">
+            残り時間: {String(hours)}時間 {String(minutes).padStart(2, "0")}分{" "}
+            {String(seconds).padStart(2, "0")}秒
+        </div>
+    );
+};
+
+export default SessionTimer;
--- resources/js/Pages/create.tsx
+++ resources/js/Pages/create.tsx
@@ -6,6 +6,8 @@ import AppHead from "../Layouts/AppHead";
 import MainForm from "@/Components/MainForm";
 import { PageProps } from "@/types";
 import { useArticleForm, FormValues } from "@/Hooks/useArticleForm";
+import SessionTimer from "@/Components/SessionTimer";
+import { useWarnOnExit } from "@/Hooks/useWarnOnExit";
 
 export default function create({ auth }: PageProps) {
     // 各値の初期値は、空文字列
@@ -23,6 +25,9 @@ export default function create({ auth }: PageProps) {
         tags: [],
         delete_image: false,
     });
+
+    // ページの再読込や遷移時に警告を表示
+    useWarnOnExit(values);
 
     // カスタムフック
     const {
@@ -45,6 +50,12 @@ export default function create({ auth }: PageProps) {
                         <h1 className="font-noto-sans-jp font-bold text-3xl mb-4 p-2 sm:p-0">
                             記事投稿
                         </h1>
+                        <SessionTimer />
+                        <div className="text-sm text-gray-500 mb-4 font-noto-sans-jp">
+                            残り時間が無くなると、入力内容がリセットされます。時間内に投稿してください。
+                            <br />
+                            また、ページの再読込をする及び別のページへ遷移すると、入力内容がリセットされます。
+                        </div>
                         <div className="py-6 max-w-7xl mx-auto sm:px-6 lg:px-8">
                             <MainForm
                                 values={values}
