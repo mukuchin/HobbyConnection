79eadde94a3c519882842ed54fca80b2d6fc6070
mukuchin <haruto.mukuno@gmail.com>
2023-10-20 18:13:53 +0900

    HandlePostTooLarge ミドルウェアの handle メソッドで、エラーメッセージをセッションに追加する部分の直前に dd() を使用して、エラーメッセージが正しくセットされているか確認

--- app/Http/Middleware/HandlePostTooLarge.php
+++ app/Http/Middleware/HandlePostTooLarge.php
@@ -10,7 +10,9 @@ class HandlePostTooLarge
 {
     public function handle(Request $request, Closure $next)
     {
+        // 画像の合計サイズが20MBを超えた場合のエラー処理
         if ($request->server('CONTENT_LENGTH') > (20 * 1024 * 1024)) { // 20MB
+            dd(['total_image_size' => '一度の投稿・更新で追加する画像の合計サイズは20MB以下にしてください。']);
             return redirect($request->fullUrl())
                 ->withInput($request->input())
                 ->withErrors(['total_image_size' => '一度の投稿・更新で追加する画像の合計サイズは20MB以下にしてください。']);
