956e4a25ed4c568d73615fff1080cb4805b48c74
mukuchin <haruto.mukuno@gmail.com>
2023-10-27 22:22:49 +0900

    database/seeders/ArticlesTableSeeder2.phpのIDをtest-user2のIDにした。

--- database/seeders/ArticlesTableSeeder2.php
+++ database/seeders/ArticlesTableSeeder2.php
@@ -12,6 +12,8 @@ class ArticlesTableSeeder2 extends Seeder
      */
     public function run(): void
     {
-        Article::factory(40)->create();
+        Article::factory(40)
+            ->forUser(\App\Models\User::find(3)) // テスト用環境のtest-user2のidは3
+            ->create();
     }
 }
