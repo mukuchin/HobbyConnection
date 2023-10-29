20b93e84f00e678480b47f22cc7ece1c52a53768
mukuchin <haruto.mukuno@gmail.com>
2023-10-29 15:06:37 +0900

    HEROKU_POSTGRESQL_PUCE_URL環境変数からデータベースの接続情報を取得し、それを使用してpgsqlのデータベース設定をsita.

--- config/database.php
+++ config/database.php
@@ -65,16 +65,14 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"] ?? '',
+            'port' => $DATABASE_URL["port"] ?? '',
+            'database' => ltrim($DATABASE_URL["path"], "/") ?? '',
+            'username' => $DATABASE_URL["user"] ?? '',
+            'password' => $DATABASE_URL["pass"] ?? '',
             'charset' => 'utf8',
             'prefix' => '',
-            'prefix_indexes' => true,
-            'search_path' => 'public',
+            'schema' => 'public',
             'sslmode' => 'prefer',
         ],
 
@@ -125,7 +123,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
         ],
 
         'default' => [
